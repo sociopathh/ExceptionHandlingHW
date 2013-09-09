08f75d4b0deeb189dec05aa6217dc32491a362b7
sociopathh <andrey.d.kostadinov@gmail.com>
2013-09-09 23:43:19 +0300

    ExceptionHandling

---
+++ 01.SquareRoot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class SquareRoot
+{
+    public static double Sqrt(double value)
+    {
+        if (value < 0)
+        {
+            throw new System.ArgumentOutOfRangeException(
+               "Sqrt for negative numbers is undefined!");
+        }
+        else
+        {
+            return Math.Sqrt(value);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter number:");
+        try
+        {
+            double n = int.Parse(Console.ReadLine());
+            double result = Sqrt(n);
+            Console.WriteLine(result);
+        }
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("Error: Invalid number");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.Error.WriteLine("Error: Invalid number - " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ 04.FileDownload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+class FileDownload
+{
+    static void Main()
+    {
+        using (WebClient webClient = new WebClient())
+        {
+            try
+            {
+                webClient.DownloadFile("http://www.devbg.org/img/Logo-BASD.jpg", "../../logo.jpg");
+            }
+
+            catch (WebException)
+            {
+                Console.Error.WriteLine("The address is invalid.");
+            }
+
+            catch (NotSupportedException)
+            {
+                Console.Error.WriteLine("The method has been called simultaneously on multiple threads.");
+            }
+        }
+    }
+}
